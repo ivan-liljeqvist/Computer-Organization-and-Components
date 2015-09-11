787343a8b3f5c51a3552c273c5b5462f5286c491
ivan-liljeqvist <ivanli@kth.se>
2015-09-11 11:06:12 +0200

    done

--- timetemplate.asm
+++ timetemplate.asm
@@ -1,6 +1,6 @@
-  # timetemplate.asm
+# timetemplate.asm
   # Written 2015 by F Lundevall
-  # Modified and improved by Ivan Liljeqvist and Filip Martinsson
+  # Ivan Liljeqvist and Filip Martinsson
   # Copyright abandonded - this file is in the public domain.
 
 .macro	PUSH (%reg)
@@ -25,7 +25,7 @@ main:
 	syscall
 	nop
 	# wait a little
-	li	$a0,2
+	li	$a0 1000
 	jal	delay
 	nop
 	# call tick
@@ -38,7 +38,6 @@ main:
 	lw	$a1,0($t0)
 	jal	time2string
 	nop
-mainReturn:
 	# print a newline
 	li	$a0,10
 	li	$v0,11
@@ -74,27 +73,48 @@ tiend:	sw	$t0,0($a0)	# save updated result
 	jr	$ra		# return
 	nop
 
-  # you can write your code for subroutine "hexasc" below this line
-  #
-  
-
 hexasc:
-	andi	$v0, $a0, 0x00000f #Mask so that we ignore everything except the last 4 bits (F is 4 bits at the end) 
+	andi	$v0, $a0, 0xf	   #Mask so that we ignore everything except the last 4 bits (F is 4 bits at the end) 
 	addi	$v0, $v0, 0x30	   #Add the start value of ASCII table
 	
 	addi 	$t1, $zero,  0x3a  #Threshold between numbers and characters
 	slt	$t2, $v0,$t1	   #Decide if letter or character
 	
 	bne 	$t2, 1, letter
+	nop
 	jr	$ra
+	nop
 	
 letter:
 	addi	$v0,$v0,7	  #If letter - jump 7 steps more to match where the letters are in the table
 	jr	$ra
-
+	nop
 		
-delay: jr $ra
-nop
+delay: 
+	PUSH $t0
+	PUSH $t1
+	
+	addi	$t0,$zero,0 #i
+	addi	$t1,$zero,22 #delay
+
+outerLoop:	
+	sub	$t0,$t0,$t0
+	ble	$a0,$zero,delayEnd
+	nop
+		subi	$a0,$a0,1
+innerLoop:
+		addi	$t0,$t0,1
+		bne	$t0,$t1,innerLoop #less than delay constant, keep loop
+		nop
+		j	outerLoop
+		nop
+	
+delayEnd:
+	POP $t1
+	POP $t0
+	
+	jr $ra
+	nop
 
 
 time2string:
@@ -104,10 +124,11 @@ time2string:
 	PUSH	$t7
 	PUSH	$t6
 
-	addi	$a1,$a1,0x0000fffff
+	andi	$a1,$a1,0x0000ffff
 	add	$t8,$zero,$a0 #COPY THE ADRESS
 	addi	$t7,$zero,0 #STRING TO RETURN
 	addi	$t6,$zero,0 #SECOND STRING TO RETURN
+	add	$t5,$zero,$ra #save return adress
 	
 	
 	
@@ -119,11 +140,12 @@ time2string:
 	PUSH	$t7
 	PUSH	$t6
 	jal	hexasc
+	nop
 	POP	$t6
 	POP	$t7
 	POP	$t8
 	
-	nop
+	
 	
 	
 	
@@ -143,10 +165,11 @@ time2string:
 	PUSH	$t7
 	PUSH	$t6
 	jal	hexasc
+	nop
 	POP	$t6
 	POP	$t7
 	POP	$t8
-	nop
+	
 	
 	#append the second character
 	sll	$t7,$t7,8	
@@ -159,10 +182,11 @@ time2string:
 	PUSH	$t7
 	PUSH	$t6
 	jal	hexasc
+	nop
 	POP	$t6
 	POP	$t7
 	POP	$t8
-	nop	
+		
 	
 	#append the first character
 	sll	$t7,$t7,8
@@ -171,7 +195,7 @@ time2string:
 	
 	##INTRODUCING NEW VARIABLE t6. t7 is now full and cant be used anymore
 	
-	#append the fourth character
+	#append the null character
 	sll	$t6,$t6,8	
 	add	$t6,$t6,0x00
 	
@@ -182,10 +206,11 @@ time2string:
 	PUSH	$t7
 	PUSH	$t6
 	jal	hexasc
+	nop
 	POP	$t6
 	POP	$t7
 	POP	$t8
-	nop
+	
 	
 	#append the fourth character
 	sll	$t6,$t6,8	
@@ -201,12 +226,8 @@ time2string:
 	PUSH	$t6
 	PUSH	$t7
 	PUSH	$t8
+	
+	
 	#GO BACK
-	j	mainReturn
-	
-	
-	
-	
-	
-	
-		
+	jr	$t5
+	nop
