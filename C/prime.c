b6e97c1e15b19b9af8e16850dbacf5e31d79ee66
ivan-liljeqvist <ivanli@kth.se>
2015-09-16 10:55:53 +0200

    C added

---
+++ C/prime.c
@@ -0,0 +1,19 @@
+/*
+ prime.c
+ By David Broman.
+ Last modified: 2015-09-15
+ This file is in the public domain.
+*/
+
+
+#include <stdio.h>
+
+int is_prime(int n){
+  return 0;
+}
+
+int main(void){
+  printf("%d\n", is_prime(11));  // 11 is a prime.      Should print 1.
+  printf("%d\n", is_prime(383)); // 383 is a prime.     Should print 1.
+  printf("%d\n", is_prime(987)); // 987 is not a prime. Should print 0.
+}
